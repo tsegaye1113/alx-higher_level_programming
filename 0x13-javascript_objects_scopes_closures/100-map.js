4eb49fe61b629703786994f94d3daef51c2c84a4
Tsegaye <tsegaye1113@gmail.com>
2023-04-11 23:12:03 +0300

    commit on 0x13-javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+/* Maps an array and multiplies each element eith it's index */
+const list = require('./100-data').list;
+console.log(list);
+console.log(list.map((element, index) => element * index));
